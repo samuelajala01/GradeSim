d47b543c2b5fed9bd6c8dea168c18b51fd905db2
Samuel <samuelajala01@gmail.com>
2024-09-27 14:43:43 +0100

    chore: change about to analytics

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <h1 className="text-white text-2xl font-bold mt-4 mb-8">GradeSim</h1>
             <nav className="space-y-2">
                 <Link to="/" className="text-white block px-4 md:px-16 py-2 hover:bg-[#111827]">Dashboard</Link>
-                <Link to="/about" className="text-white block px-4 md:px-16 py-2 hover:bg-[#111827]">About</Link>
+                <Link to="/analytics" className="text-white block px-4 md:px-16 py-2 hover:bg-[#111827]">Analytics</Link>
                 <Link to="/contact" className="text-white block px-4 md:px-16 py-2 hover:bg-[#111827]">Logout</Link>
             </nav>
         </div>
